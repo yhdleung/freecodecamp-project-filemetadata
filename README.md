d4d05a5f9b2f74b91143eabd8e60cc0f2f83c04f
Dick Leung <yhd.leung@gmail.com>
2022-05-14 13:24:22 +0800

    implement API using multer package to return upload file metadata

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # File Metadata Microservice
 
 This is the boilerplate for the File Metadata Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice
+
+
+### Return metadata of the upload file
+> POST /api/fileanalyse  
+
+sample response
+
+```json
+{"name":"sample.pdf","type":"application/pdf","size":703150}
+```
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer  = require('multer');
 require('dotenv').config()
 
-var app = express();
+const upload = multer({ dest: 'uploads/' });
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,8 +13,13 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post("/api/fileanalyse", upload.single('upfile'), function (req, res, next) {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
